e591da8317cb6f8fe158e641ce5445a60e93e29e
AmanR404 <107179665+AmanR404@users.noreply.github.com>
2025-06-02 13:51:16 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
     <header className='flex justify-between px-8 h-12 bg-[url("background.jpg")]'>
         <div className='flex gap-8 items-center'>
             <span className='px-6 py-1 hidden md:inline-block bg-white rounded-full hover:bg-gray-300 hover:cursor-pointer'>Patient System</span>
-            <img onClick={()=>sidebarSwitcher()} className='w-6' src="hamburger.svg" alt="" />
+            <img onClick={()=>sidebarSwitcher()} className='w-6 cursor-pointer' src="hamburger.svg" alt="" />
         </div>
         <div className='flex flex-col sm:flex-row gap-1 sm:gap-4'>
             <button className='hidden sm:inline-block h-8 mt-2 px-6 text-sm sm:text-md bg-white rounded-full hover:bg-gray-300 hover:cursor-pointer' onClick={()=>navigate('/services')}>Services</button>
